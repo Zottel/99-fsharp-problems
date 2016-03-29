86487b23405da2478b004fd2429bb7075088501b
Michał Małolepszy <michal.malolepszy@altkom.pl>
2016-03-29 13:20:45 +0200

    Problem 27: Group the elements of a set into disjoint subsets.

---
+++ 01-Lists/27-GroupElements.fsx
@@ -0,0 +1,21 @@
+module Lists.GroupElements
+
+#load "26-Combinations.fsx"
+open Lists.Combinations
+
+let listDiff list1 list2 = 
+    Set.toList ((Set.ofList list1) - (Set.ofList list2))
+    
+let group3 list = 
+    [for a in combinations 2 list do
+        let noA = listDiff list a
+        for b in combinations 3 noA do
+            yield [a] @ [b] @ [(listDiff noA b)]]
+    
+let rec group groups list =
+    match groups, list with
+    | [],_ -> [[]]
+    | head::tail, list -> 
+        [for a in combinations head list do
+            for b in group tail (listDiff list a) do
+                yield a :: b]
